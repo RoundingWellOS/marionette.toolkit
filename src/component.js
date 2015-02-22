6d07032b8caa220ca908fa4668b00fe568f6f321
paulfalgout <paul.falgout@roundingwell.com>
2015-02-22 03:15:31 -0600

    Finish Component jsdoc

--- src/component.js
+++ src/component.js
@@ -14,7 +14,7 @@ var Component = StateClass.extend({
 
   /**
    * The view class to be managed.
-   * @type {Marionette.ItemView|Marionette.CollectionView|Marionette.CompositeView|Marionette.LayoutView}
+   * @type {Mn.ItemView|Mn.CollectionView|Mn.CompositeView|Mn.LayoutView}
    * @default Marionette.ItemView
    */
   ViewClass: Marionette.ItemView,
@@ -39,7 +39,7 @@ var Component = StateClass.extend({
    * @constructs Component
    * @param {Object} [stateAttrs] - Attributes to set on the state model.
    * @param {Object} [options] - Settings for the component.
-   * @param {Marionette.ItemView|Marionette.CollectionView|Marionette.CompositeView|Marionette.LayoutView} [options.ViewClass] - The view class to be managed.
+   * @param {Mn.ItemView|Mn.CollectionView|Mn.CompositeView|Mn.LayoutView} [options.ViewClass] - The view class to be managed.
    * @param {String} [options.viewEventPrefix] - Used as the prefix for events forwarded from the component's view to the component
    * @param {Object} [options.viewOptions] - The view class to be managed.
    * @param {Marionette.Region=} [options.region] - The region to show the component in.
@@ -80,14 +80,37 @@ var Component = StateClass.extend({
     this.setState(stateAttrs, { silent: true });
   },
 
+  /**
+   * Set the Component's region and then show it.
+   *
+   * @public
+   * @method showIn
+   * @memberOf Component
+   * @param {Marionette.Region} region - The region for the component
+   * @returns {Component}
+   */
   showIn: function(region) {
     if(region) {
         this.region = region;
     }
 
     this.show();
+
+    return this;
   },
 
+  /**
+   * Show the Component in its region.
+   *
+   * @public
+   * @event Component#before:show
+   * @event Component#show
+   * @throws ComponentShowError - Thrown if component has already been show.
+   * @throws ComponentRegionError - Thrown if component has no defined region.
+   * @method show
+   * @memberOf Component
+   * @returns {Component}
+   */
   show: function(){
     if(this._isShown) {
         throw new Marionette.Error({
@@ -113,8 +136,20 @@ var Component = StateClass.extend({
     this._isShown = true;
 
     this.triggerMethod('show');
+
+    return this;
   },
 
+  /**
+   * Shows or re-shows a newly built view in the component's region
+   *
+   * @public
+   * @event Component#before:render:view
+   * @event Component#render:view
+   * @method renderView
+   * @memberOf Component
+   * @returns {Component}
+   */
   renderView: function(){
     this.view = this.buildView();
 
@@ -132,13 +167,20 @@ var Component = StateClass.extend({
     this._shouldDestroy = true;
 
     this.triggerMethod('render:view', this.view);
+
+    return this;
   },
 
+  /**
+   * Proxies the ViewClass's viewEvents to the Component itself
+   * Similar to CollectionView childEvents
+   * (http://marionettejs.com/docs/v2.3.2/marionette.collectionview.html#collectionviews-childevents)
+   *
+   * @private
+   * @method _proxyViewEvents
+   * @memberOf Component
+   */
   _proxyViewEvents: function(){
-    // Proxies the ViewClass's viewEvents to the Component itself
-    // similar to CollectionView childEvents
-    // (http://marionettejs.com/docs/v2.3.2/marionette.collectionview.html#collectionviews-childevents)
-
     var prefix = this.getOption('viewEventPrefix');
 
     this.view.on('all', function() {
@@ -152,28 +194,76 @@ var Component = StateClass.extend({
     }, this);
   },
 
-  // mixin _state_model from StateObject with any other viewOptions
+  /**
+   * Mixin stateModel from StateClass with any other viewOptions
+   *
+   * @public
+   * @abstract
+   * @method mixinOptions
+   * @memberOf Component
+   * @param {Object=} options - Additional options to mixin
+   * @returns {Object}
+   */
   mixinOptions: function(options){
     return _.extend({ model: this.getState() }, this.viewOptions, options);
   },
 
+  /**
+   * Get the component ViewClass.
+   * If you need a dynamic ViewClass override this function
+   *
+   * @public
+   * @abstract
+   * @method getViewClass
+   * @memberOf Component
+   * @returns {Mn.ItemView|Mn.CollectionView|Mn.CompositeView|Mn.LayoutView}
+   */
+  getViewClass: function(){
+    return this.ViewClass;
+  }
+
+  /**
+   * Builds the view class with mixed in options
+   *
+   * @public
+   * @abstract
+   * @method buildView
+   * @memberOf Component
+   * @returns {Mn.ItemView|Mn.CollectionView|Mn.CompositeView|Mn.LayoutView}
+   */
   buildView: function() {
-    return new this.ViewClass(this.mixinOptions());
+    var ViewClass = this.getViewClass();
+
+    return new ViewClass(this.mixinOptions());
   },
 
+  /**
+   * Destroys Component and empties its region.
+   *
+   * @private
+   * @method _destroy
+   * @memberOf Component
+   */
   _destroy: function(){
     // apply destroy first for listener cleanup
     Marionette.StateObject.prototype.destroy.apply(this, arguments);
 
-    this.region.empty();
+    if(this.region) {
+      this.region.empty();
+    }
   },
 
+  /**
+   * If the component should be destroyed, destroy it.
+   *
+   * @public
+   * @method destroy
+   * @memberOf Component
+   */
   destroy: function(){
     if(this._shouldDestroy) {
         this._destroy();
     }
-
-    return this;
   }
 });
 
