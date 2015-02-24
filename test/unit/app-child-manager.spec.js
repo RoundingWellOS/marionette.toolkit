5264e81d96013662a7c156792ca410b979e99b0f
Terrence L. Jones <terrence.jones@roundingwell.com>
2015-02-24 14:55:00 -0600

    buildApp fix for Issue 35
    Refactor code to check first to see if AppClass is a function. Also added tests.

---
+++ test/unit/app-child-manager.spec.js
@@ -0,0 +1,26 @@
+import '../../src/marionette.toolkit';
+
+describe('App Manager', function() {
+  describe('when passing a class to buildApp', function() {
+    beforeEach(function() {
+      this.myApp = new Marionette.Toolkit.App();
+    });
+
+    it('should return an instance of the class', function() {
+      var foo = this.myApp.buildApp(Marionette.Toolkit.App);
+
+      expect(foo).to.be.instanceOf(Marionette.Toolkit.App);
+    });
+
+    describe('when passing an object to buildApp', function() {
+      it('should return an instance of obj.AppClass', function() {
+        var bar = { AppClass: Marionette.Toolkit.App };
+        var foo = this.myApp.buildApp(bar);
+
+        expect(foo).to.be.instanceOf(bar.AppClass);
+      });
+    });
+
+  });
+
+});
