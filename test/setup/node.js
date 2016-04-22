382a627ad33d4a7fe6c75f6d190cfe78b4e2b43c
Terrence L. Jones <terrence.jones@roundingwell.com>
2016-04-22 09:58:57 -0500

    Update testing process
    Change jsdom related items due to the version update to jsdom. Also remove browerify due to its replacement by rollup.

--- test/setup/node.js
+++ test/setup/node.js
@@ -1,16 +1,15 @@
 if(!global.document || !global.window) {
   const jsdom = require('jsdom').jsdom;
 
-  global.document = jsdom('<html><head><script></script></head><body><div id="testDiv"></div></body></html>', null, {
+  global.document = jsdom('<html><head><script></script></head><body><div id="testDiv"></div></body></html>', {
     FetchExternalResources: ['script'],
     ProcessExternalResources: ['script'],
     MutationEvents: '2.0',
     QuerySelector: false
   });
 
-  global.window = document.parentWindow;
+  global.window = document.defaultView;
   global.navigator = global.window.navigator;
-  global.location = global.window.location;
 }
 
 global.$ = global.jQuery = require('jquery');
@@ -19,5 +18,5 @@ global.chai = require('chai');
 global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
 
-require('babel/register');
+require('babel-register');
 require('./setup')();
