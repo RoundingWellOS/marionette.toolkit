1299bfb451705adec0376c05488dc35d60212dd3
Terrence L. Jones <terrenceljones805@gmail.com>
2016-05-11 13:09:50 -0500

    Create mixins for certain functionality

--- src/mixins/state.js
+++ src/mixins/state.js
@@ -2,15 +2,13 @@ import _ from 'underscore';
 import Backbone from 'backbone';
 import Marionette from 'backbone.marionette';
 
-/**
- * Marionette.Object with a Backbone.Model for keeping state.
+ /**
+ * This provides methods used for keeping state using a Backbone.Model. It's meant to
+ * be used with either a Marionette.Object or Backbone.View.
  *
- * @public
- * @class StateClass
- * @memberOf Toolkit
- * @memberOf Marionette
+ * @mixin
  */
-const StateClass = Marionette.Object.extend({
+export default {
 
   /**
    * The model class for _stateModel.
@@ -21,16 +19,14 @@ const StateClass = Marionette.Object.extend({
 
   /**
    * @public
-   * @constructs StateClass
+   * @method initState
    * @param {Object} [options] - Settings for the stateClass.
    * @param {Object} [options.stateEvents] - Event hash bound from _stateModel to stateClass.
    * @param {Backbone.Model} [options.StateModel] - Model class for _stateModel.
    */
-  constructor: function(options) {
-    options = _.extend({}, options);
-
+  initState(options = {}) {
     // Make defaults available to this
-    _.extend(this, _.pick(options, ['StateModel', 'stateEvents', 'stateDefaults']));
+    this.mergeOptions(options, ['StateModel', 'stateEvents', 'stateDefaults']);
 
     const StateModel = this._getStateModel(options);
 
@@ -38,8 +34,6 @@ const StateClass = Marionette.Object.extend({
 
     // Bind events from the _stateModel defined in stateEvents hash
     this.bindEntityEvents(this._stateModel, _.result(this, 'stateEvents'));
-
-    Marionette.Object.call(this, options);
   },
 
   /**
@@ -50,16 +44,13 @@ const StateClass = Marionette.Object.extend({
    * @private
    * @method _getStateModel
    * @param {Object} [options] - Options that can be used to determine the StateModel.
-   * @memberOf StateClass
    * @returns {Backbone.Model}
    */
-  _getStateModel: function(options) {
-    const StateModel = this.getOption('StateModel');
-
-    if(StateModel.prototype instanceof Backbone.Model || StateModel === Backbone.Model) {
-      return StateModel;
-    } else if(_.isFunction(StateModel)) {
-      return StateModel.call(this, options);
+  _getStateModel(options) {
+    if(this.StateModel.prototype instanceof Backbone.Model || this.StateModel === Backbone.Model) {
+      return this.StateModel;
+    } else if(_.isFunction(this.StateModel)) {
+      return this.StateModel.call(this, options);
     }
 
     throw new Marionette.Error({
@@ -73,13 +64,12 @@ const StateClass = Marionette.Object.extend({
    *
    * @public
    * @method setState
-   * @memberOf StateClass
    * @param {String|Object} key - Attribute name or Hash of any number of key value pairs.
    * @param {*} [value] - Attribute value if key is String, replaces options param otherwise.
    * @param {Object} [options] - Backbone.Model options.
    * @returns {Backbone.Model} - The _stateModel
    */
-  setState: function() {
+  setState() {
     return this._stateModel.set.apply(this._stateModel, arguments);
   },
 
@@ -88,11 +78,10 @@ const StateClass = Marionette.Object.extend({
    *
    * @public
    * @method getState
-   * @memberOf StateClass
    * @param {String} [attr] - Attribute name of stateModel.
    * @returns {Backbone.Model|*} - The _stateModel or the attribute value of the _stateModel
    */
-  getState: function(attr) {
+  getState(attr) {
     if(!attr) {
       return this._stateModel;
     }
@@ -101,17 +90,12 @@ const StateClass = Marionette.Object.extend({
   },
 
   /**
-   * Destroy the stateClass and clean up any listeners on the _stateModel.
+   * Clean up any listeners on the _stateModel.
    *
    * @public
-   * @method destroy
-   * @memberOf StateClass
+   * @method destroyState
    */
-  destroy: function() {
+  destroyState() {
     this._stateModel.stopListening();
-
-    Marionette.Object.prototype.destroy.apply(this, arguments);
   }
-});
-
-export default StateClass;
+};
