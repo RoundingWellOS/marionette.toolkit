b054f1725f656b9fd5f2461cf5c7ba783ddc1b5e
Terrence L. Jones <terrenceljones805@gmail.com>
2016-05-20 10:48:16 -0500

    Add StateMixin utility function
    Also move noconflict spec to new toolkit spec

--- test/unit/noconflict.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Marionette.Toolkit.noConflict()', function() {
-  describe('when calling noConflict', function() {
-    it('should make Marionette.Toolkit return undefined', function() {
-      const preNoConflict = Marionette.Toolkit.noConflict();
-      expect(Marionette.Toolkit).to.equal(undefined);
-      Marionette.Toolkit = preNoConflict;
-    });
-  });
-});
